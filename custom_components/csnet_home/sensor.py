3f549b343997d245b2d4b8c6687aa8ab546b34f0
CrazYoshi <mar862002@hotmail.com>
2025-04-02 21:35:00 +0200

    multiple sensors (#37)
    Co-authored-by: Marco Mornati <139323+mmornati@users.noreply.github.com>

--- custom_components/csnet_home/sensor.py
+++ custom_components/csnet_home/sensor.py
@@ -7,6 +7,8 @@ from homeassistant.core import callback
 from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
+from homeassistant.components.climate.const import HVACMode
+from homeassistant.const import STATE_ON, STATE_OFF
 
 from .const import DOMAIN
 from .coordinator import CSNetHomeCoordinator
@@ -22,56 +24,59 @@ async def async_setup_entry(hass, entry, async_add_entities):
     if not coordinator:
         _LOGGER.error("No coordinator instance found!")
         return None
-    # Add sensors based on the coordinator's data
-    async_add_entities(
-        [
-            CSNetHomeSensor(
-                coordinator,
-                sensor_data=sensor_data,
-                common_data=coordinator.get_common_data()["device_status"][
-                    sensor_data["device_id"]
-                ],
-            )
-            for sensor_data in coordinator.get_sensors_data()
-        ]
-    )
+
+    sensors = []
+    for sensor_data in coordinator.get_sensors_data():
+        common_data = coordinator.get_common_data()["device_status"][sensor_data["device_id"]]
+
+        sensors.append(CSNetHomeSensor(coordinator, sensor_data, common_data, "current_temperature", "temperature", UnitOfTemperature.CELSIUS))
+        sensors.append(CSNetHomeSensor(coordinator, sensor_data, common_data, "setting_temperature", "temperature", UnitOfTemperature.CELSIUS))
+        sensors.append(CSNetHomeSensor(coordinator, sensor_data, common_data, "mode", "enum"))
+        sensors.append(CSNetHomeSensor(coordinator, sensor_data, common_data, "on_off", "enum"))
+        sensors.append(CSNetHomeSensor(coordinator, sensor_data, common_data, "doingBoost", "binary"))
+    
+    async_add_entities(sensors)
 
 
 class CSNetHomeSensor(CoordinatorEntity, Entity):
     """Representation of a sensor from the CSNet Home integration."""
 
-    def __init__(self, coordinator: CSNetHomeCoordinator, sensor_data, common_data):
+    def __init__(self, coordinator: CSNetHomeCoordinator, sensor_data, common_data, key, device_class=None, unit=None):
         """Initialize the sensor."""
         super().__init__(coordinator)
         self._coordinator = coordinator
         self._sensor_data = sensor_data
         self._common_data = common_data
-        self._name = f"{sensor_data['device_name']} {sensor_data['room_name']}"
-        self._current_temperature = sensor_data["current_temperature"]
-        self._planned_temperature = sensor_data["setting_temperature"]
+        self._key = key
+        self._device_class = device_class
+        self._unit = unit
+        self._name = f"{sensor_data['device_name']} {sensor_data['room_name']} {key}"
         _LOGGER.debug("Configuring Sensor %s", self._name)
 
     @property
     def state(self):
         """Return the current temperature as the state of the sensor."""
-        return self._current_temperature
+        value = self._sensor_data.get(self._key)
+        if self._key == "mode":
+            if value == 0:
+                return HVACMode.COOL
+            elif value == 1:
+                return HVACMode.HEAT
+            else:
+                return HVACMode.OFF
+        elif self._key == "on_off":
+            return STATE_ON if value == 1 else STATE_OFF
+        return value
 
     @property
     def device_class(self):
         """Return the device class of the sensor."""
-        return "temperature"
+        return self._device_class
 
     @property
     def unit_of_measurement(self):
         """Return the unit of measurement for the sensor."""
-        return UnitOfTemperature.CELSIUS
-
-    @property
-    def extra_state_attributes(self):
-        """Return the planned temperature as an extra attribute."""
-        return {
-            "planned_temperature": self._planned_temperature,
-        }
+        return self._unit
 
     @callback
     def _handle_coordinator_update(self) -> None:
@@ -84,10 +89,8 @@ class CSNetHomeSensor(CoordinatorEntity, Entity):
             ),
             None,
         )
-        self._common_data = self._coordinator.get_common_data()
-        self._current_temperature = self._sensor_data["current_temperature"]
-        self._planned_temperature = self._sensor_data["setting_temperature"]
-        self.async_write_ha_state()
+        if self._sensor_data:
+            self.async_write_ha_state()
 
     @property
     def device_info(self) -> DeviceInfo:
@@ -108,11 +111,11 @@ class CSNetHomeSensor(CoordinatorEntity, Entity):
     @property
     def name(self) -> str:
         """Return the name of the sensor."""
-        return f"{self._sensor_data['device_name']}-{self._sensor_data['room_name']}"
+        return self._name
 
     @property
     def unique_id(self) -> str:
         """Return unique id."""
         # All entities must have a unique id.  Think carefully what you want this to be as
         # changing it later will cause HA to create new entities.
-        return f"{DOMAIN}-{self._sensor_data['room_name']}"
+        return f"{DOMAIN}-{self._sensor_data['room_name']}-{self._key}"
